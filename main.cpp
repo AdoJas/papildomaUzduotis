ec3d39893e11487a367472ff6d859139f3018476
AdoJas <147920380+AdoJas@users.noreply.github.com>
2024-05-31 22:30:04 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -13,29 +13,52 @@ std::string transform(std::string& input){
     return lower_str;
 }
 
+std::string removePunctuation(std::string& input){
+    std::string result = "";
+    for(char c : input){
+        if(isalnum(c) || c == ' '){
+            result += c;
+        }
+    }
+    return result;
+}
 
 int main() {
     std::ifstream inputFile("input.txt");
     std::ofstream outputFile("output.txt");
     std::ofstream outputUrl("urls.txt");
 
-    std::map<std::string, int> words;
-    std::map<std::string, std::unordered_set<int>> wordLines;
+    std::map<std::string, int> words; //Fiksuoja kiek kartu pasikartoja zodis
+    std::map<std::string, std::unordered_set<int>> wordLines; //Fiksuoja koks key[zodis] turi pasikartojima kurioj linijoj
+
     std::vector<std::string> urlVector;
+
     std::regex ulrRegex(R"((http[s]?://[^\s]+|www\.[^\s]+|[^\s]+\.[a-z]{2,}))");
+
     std::string line = "";
+
     int lineNumber = 0;
+
     if(!inputFile.is_open()){
         std::cerr << "Nepavyko atidaryti failo" << std::endl;
     }
 
     while(std::getline(inputFile, line)){
         ++lineNumber;
-        getline(inputFile, line);
+        std::istringstream lineStream(line);
+        std::string word;
 
+        while(lineStream >> word){
+            std::string lowerWord = transform(word);
+            std::string cleanWord = removePunctuation(lowerWord);
+            if(std::regex_match(cleanWord, ulrRegex)){
+                urlVector.push_back(cleanWord);
+            } else {
+                words[cleanWord]++;
+                wordLines[cleanWord].insert(lineNumber);
+            }
+        }
     }
-
-
     return 0;
 }
 
